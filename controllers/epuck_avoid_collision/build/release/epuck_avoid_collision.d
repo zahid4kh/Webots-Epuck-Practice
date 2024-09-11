84962f81ebd434156bb57f111de4efd88a5762a3
zahid4kh <halilzahid@gmail.com>
2024-09-11 12:26:16 +0200

    collision avoidance

---
+++ controllers/epuck_avoid_collision/build/release/epuck_avoid_collision.d
@@ -0,0 +1,6 @@
+build/release/epuck_avoid_collision.o: epuck_avoid_collision.c \
+ /usr/local/webots/include/controller/c/webots/robot.h \
+ /usr/local/webots/include/controller/c/webots/types.h \
+ /usr/local/webots/include/controller/c/webots/nodes.h \
+ /usr/local/webots/include/controller/c/webots/motor.h \
+ /usr/local/webots/include/controller/c/webots/distance_sensor.h
--- controllers/epuck_avoid_collision/epuck_avoid_collision.c
+++ controllers/epuck_avoid_collision/epuck_avoid_collision.c
@@ -12,51 +12,70 @@
  */
 #include <webots/robot.h>
 #include <webots/motor.h>
+#include <webots/distance_sensor.h>
 
-/*
- * You may want to add macros here.
- */
 #define TIME_STEP 64
+#define MAX_SPEED 6.28
 
-/*
- * This is the main program.
- * The arguments of the main function can be specified by the
- * "controllerArgs" field of the Robot node
- */
 int main(int argc, char **argv) {
   /* necessary to initialize webots stuff */
-  wb_robot_init();
+  //initializing webots API
+  wb_robot_init(); 
 
-  /*
-   * You should declare here WbDeviceTag variables for storing
-   * robot devices like this:
-   *  WbDeviceTag my_sensor = wb_robot_get_device("my_sensor");
-   *  WbDeviceTag my_actuator = wb_robot_get_device("my_actuator");
-   */
+  int i;
+  
+  WbDeviceTag ps[8];
 
-  /* main loop
-   * Perform simulation steps of TIME_STEP milliseconds
-   * and leave the loop when the simulation is over
-   */
-  while (wb_robot_step(TIME_STEP) != -1) {
-    /*
-     * Read the sensors :
-     * Enter here functions to read sensor data, like:
-     *  double val = wb_distance_sensor_get_value(my_sensor);
-     */
-
-    /* Process sensor data here */
-
-    /*
-     * Enter here functions to send actuator commands, like:
-     * wb_motor_set_position(my_actuator, 10.0);
-     */
+  char ps_names[8][4] = {
+    "ps0", "ps1", "ps2", "ps3", 
+    "ps4", "ps5", "ps6", "ps7"
   };
 
-  /* Enter your cleanup code here */
+  for (i = 0; i < 8; i++){
+    ps[i] = wb_robot_get_device(ps_names[i]);
+    wb_distance_sensor_enable(ps[i], TIME_STEP);
+  }
 
-  /* This is necessary to cleanup webots resources */
+  WbDeviceTag left_motor = wb_robot_get_device("left wheel motor");
+  WbDeviceTag right_motor = wb_robot_get_device("right wheel motor");
+  wb_motor_set_position(left_motor, INFINITY);
+  wb_motor_set_position(right_motor, INFINITY);
+  wb_motor_set_velocity(left_motor, 0.0);
+  wb_motor_set_velocity(right_motor, 0.0);
+
+  while (wb_robot_step(TIME_STEP) != -1) {
+    // reading sensor outputs
+    double ps_values[8];
+    for (i = 0; i < 8; i++){
+      ps_values[i] = wb_distance_sensor_get_value(ps[i]);
+      }
+
+  // detect obstacles
+  bool right_obstacle = ps_values[0] > 80.0 || ps_values[1] > 80.0 || ps_values[2] > 80.0;
+  bool left_obstacle = ps_values[5] > 80.0 || ps_values[6] > 80.0 || ps_values[7] > 80.0;
+
+  // initializing motor speeds
+  double left_speed = 0.5 * MAX_SPEED;
+  double right_speed = 0.5 * MAX_SPEED;
+
+  if (left_obstacle){
+    // turning right
+    left_speed = 0.5 * MAX_SPEED;
+    right_speed = -0.5 * MAX_SPEED;
+  }
+  else if (right_obstacle){
+    // turning left
+    left_speed = -0.5 * MAX_SPEED;
+    right_speed = 0.5 * MAX_SPEED;
+  }
+
+  // writing updated wheel values in case of obstacles
+  wb_motor_set_velocity(left_motor, left_speed);
+  wb_motor_set_velocity(right_motor, right_speed);
+
+  };
+
+  // cleanup webots API
   wb_robot_cleanup();
-
   return 0;
 }
